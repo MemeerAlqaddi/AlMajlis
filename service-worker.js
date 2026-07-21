19f00c9caeeed6045c4f83e4a203c705d27b1648
MemeerAlqaddi <alchaar98@gmail.com>
2026-07-21 00:48:58 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='al-majlis-v14';
+const CACHE='al-majlis-v16';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./al-majlis-icon.svg','./apple-touch-icon.png','./icon-192.png','./icon-512.png','./icon-maskable-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
